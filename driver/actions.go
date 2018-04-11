09e5e462ab2b85f4291a76741a923b0d36b908a4
Jonasglien <Jonasglien2@gmail.com>
2018-04-11 11:25:18 +0200

    first commit

---
+++ driver/actions.go
@@ -0,0 +1,159 @@
+package driver
+
+import ("fmt"
+	"time"
+)
+import state "../state"
+
+func timeout_timer(cancel <- chan bool, timeout chan <- bool) {
+	for i := 0; i < 10; i++ {
+		time.Sleep(500*time.Millisecond)
+		select {
+		case <- cancel:
+			return
+
+		default:
+		}
+	}
+	timeout <- true
+}
+
+func Button_manager(b <- chan ButtonEvent, save chan <- bool, e *state.Elevator) {
+	for {
+		select {
+		case event := <- b:
+			if (event.Button == BT_Cab) {
+				order := Button_event_to_order(event)
+				Order_accept(e, order)
+				
+				fmt.Println("-------------------------------")
+				fmt.Println("Button - Order floor: ", event.Floor)
+				fmt.Println("Button - Elevator stops: ", e.Stops)
+				fmt.Println("Button - Current motor direction: ", e.Dir)
+				if (e.Dir == state.MD_Stop) {
+					move_to_next_floor(e)
+				}
+			} else if (event.Button == BT_Hallup || BT_Halldown) { 
+			
+			        network.broadcast_state()
+			        //poll states
+			        //time.Sleep(100*Millisecond)
+				
+				cost_local := driver.Evaluate(e)
+				cost_e2 := driver.Evaluate(e2)
+				cost_e3 := driver.Evaluate(e3)
+			        Choose_elevator()
+				
+				if (network.is_Alive)
+					wd_timeout := make(chan bool)
+					wd_cancel := make(chan bool)
+					ack_1 := make(chan bool)
+					ack_2 := make(chan bool)
+					
+					Activate_timeout(ch_cancel, ch_timeout)
+					network.Ack_listener1(ack1)
+					netowrk.Ack_listener2(ack2)
+					select {
+					case <- ack_1:
+						wd_cancel <- true
+						break
+					case <- ack_2:
+						wd_cancel <- true
+						break
+					case <- wd_timeout
+						Order_accept(e, order)
+					}
+				else {
+					Order_accept(e, order)
+				}
+				//TODO: Broadcast corresponding order
+				//TODO: Evaluate all elevators and decide which one taking the order
+				//TODO: Update corresponding elevator struct -> Stops[event.Floor]
+			} 
+			
+		case (stop-button):
+		        //TODO: stop elevator
+		        //TODO: broadcast to others that elevator has stopped and do not take orders
+		        //TODO: Create bool for stop-unstop
+		        //TODO: Elevator are set as lost until stop button is pressed again and elev is set alive
+		        
+		case obstruction: 
+		        //TODO: acknowledge is turned off, other elevators MUST take orders
+		        //TODO: Ack fails such that elevators are set as lost
+			
+			save <- true
+		}
+	}
+}
+
+func Event_manager(f <- chan int, save chan <- bool, ep *state.Elevator) {
+	prev_floor := -1
+	for {
+		select {
+		case floor := <- f:
+			if (floor != prev_floor) {
+				SetFloorIndicator(floor)
+				ep.CurrentFloor = floor
+				fmt.Println("Current floor: ", ep.CurrentFloor)
+				if (ep.Stops[floor] > 0) {
+					fmt.Println("Stopping at floor ", floor)
+					SetMotorDirection(state.MD_Stop)
+					SetDoorOpenLamp(true)
+					ep.Stops[floor] = 0
+					// fmt.Println("Elevator stops: ", ep.Stops)
+					time.Sleep(5*time.Second)
+					SetDoorOpenLamp(false)
+				}
+				move_to_next_floor(ep)
+			}
+			save <- true
+			prev_floor = floor
+		}
+	}
+}
+
+func Find_next_stop(e *state.Elevator) state.MotorDirection {
+	var direction state.MotorDirection = state.MD_Stop
+	if (e.Dir == state.MD_Up) {
+		for i := e.CurrentFloor; i < 4; i++ {
+			// fmt.Println("Percieved elevator stops: ", e.Stops)
+			if (e.Stops[i] > 0) {
+				direction = state.MD_Up
+				fmt.Println("continuing up")
+				return direction
+			}
+		}
+		for i := e.CurrentFloor; i >= 0; i-- {
+			if (e.Stops[i] > 0) {
+				direction = state.MD_Down
+				fmt.Println("turning down")
+				e.Dir = direction
+				return direction
+			}
+		}
+	} else {
+		for i := e.CurrentFloor; i >= 0; i-- {
+			if (e.Stops[i] > 0) {
+				direction = state.MD_Down
+				fmt.Println("continuing down")
+				return direction
+			}
+		}
+		for i := e.CurrentFloor; i < 4; i++ {
+			if (e.Stops[i] > 0) {
+				direction = state.MD_Up
+				fmt.Println("turning up")
+				e.Dir = direction
+				return direction
+			}
+		}
+	}
+	fmt.Println("No pending orders. Stopping")
+	e.Dir = state.MD_Stop
+	return direction
+}
+
+func move_to_next_floor(elevator *state.Elevator) {
+	motor_direction := Find_next_stop(elevator)
+	SetMotorDirection(motor_direction)
+}
---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import ("fmt"
+	//"./order"
+	"./driver"
+	"./state"
+	"os/exec"
+	"time"
+	//"./bcast"
+	"./network"
+)
+
+
+func main() {
+	ElevatorServer := exec.Command("gnome-terminal", "-x", "sh", "-c", "ElevatorServer;")
+	err := ElevatorServer.Start()
+	state.Check(err)
+	time.Sleep(500*time.Millisecond)
+	
+	var elevator = state.Elevator{}
+	var elevator2 = state.Elevator{}
+	var elevator3 = state.Elevator{}
+	state.Load(&elevator)
+	
+	fmt.Println("--------------------------")
+	fmt.Println("--------------------------")
+	fmt.Println("    STARTING ELEVATOR     ")
+	fmt.Println("--------------------------")
+	fmt.Println("--------------------------")
+	
+	ch_floors  := make(chan int)
+	ch_buttons := make(chan driver.ButtonEvent)
+	ch_obstr   := make(chan bool)
+	ch_stop    := make(chan bool)
+	ch_newstate:= make(chan bool)
+	ch_bcast   := make(chan state.Elevator)
+	//ch_listen  := make(chan bool)
+	ch_exit	   := make(chan bool)
+	ack_wd1_reset   := make(chan bool)
+	ack_wd2_reset  := make(chan bool)
+
+
+	driver.Init("localhost:15657", 4)
+	go driver.Button_manager(ch_buttons, ch_newstate, &elevator)
+	go driver.Event_manager(ch_floors, ch_newstate, &elevator)
+	go driver.PollButtons(ch_buttons)
+	go driver.PollFloorSensor(ch_floors)
+	go driver.PollObstructionSwitch(ch_obstr)
+	go driver.PollStopButton(ch_stop)
+	
+	go state.Save(ch_newstate, &elevator)
+	
+	go network.Broadcast_state(ch_bcast)
+	go network.Poll_remote_state(&elevator2)
+	go network.Poll_remote_state2(&elevator3)
+	go network.Ack_listener1(ack_wd1_reset)
+	go network.Ack_listener2(ack_wd2_reset)
+	go network.Ack_broadcast()
+	//ch_listen <- true
+	for {
+		ch_bcast <- elevator
+		fmt.Println("Remote currently: ", elevator2)
+		time.Sleep(5*time.Second)
+	}
+	
+	<- ch_exit
+}
